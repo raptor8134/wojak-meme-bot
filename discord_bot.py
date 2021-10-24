cb3ce6f25694722e345607f730a75a0ca72c97b0
raptor8134 <jamesnotley@gmail.com>
2021-10-23 23:41:21 -0700

    other memes working on discord bot now

--- discord_bot.py
+++ discord_bot.py
@@ -10,6 +10,16 @@ def bot_url():
     permissions = getenv("D_PERMISSIONS")
     print("https://discord.com/api/oauth2/authorize?client_id=" + client_id + "&scope=bot&permissions=" + permissions)
 
+def discord_meme(memes, meme, arguments):
+    text = ' '.join(arguments)
+    #file = io.BytesIO()
+    #memes[meme](text).save(file, "jpeg")
+    #image = discord.File(file)
+    memes[meme](text).save("tmp.jpg")
+    image = discord.File("tmp.jpg")
+    return image
+
+
 def discordbot(memes):
     bot = commands.Bot(command_prefix="!")
     token = getenv("D_TOKEN")
@@ -18,11 +28,24 @@ def discordbot(memes):
     async def on_ready():
         print("Spinning up ...")
 
-    @bot.command(aliases=["rebuke"])
+    @bot.command()
     async def soyjack(ctx, *args):
-        text = " ".join(args)
-        angrysoyjack(text).save("tmp.jpg")
-        image = discord.File("tmp.jpg")
+        image = discord_meme(memes, 'angrysoyjack', args)
+        await ctx.send(file=image)
+
+    @bot.command()
+    async def gigachad(ctx, *args):
+        image = discord_meme(memes, 'gigachad', args)
+        await ctx.send(file=image)
+
+    @bot.command()
+    async def chadyes(ctx, *args):
+        image = discord_meme(memes, 'chadyes', args)
+        await ctx.send(file=image)
+
+    @bot.command()
+    async def chadno(ctx, *args):
+        image = discord_meme(memes, 'chadno', args)
         await ctx.send(file=image)
 
     bot.run(token)

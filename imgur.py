b3a89080a26189b7ce27bc86904f9588818c21f4
raptor8134 <jamesnotley@gmail.com>
2021-12-27 05:38:37 +0000

    Made image caching safe for multi-threading on the reddit bot

--- imgur.py
+++ imgur.py
@@ -6,13 +6,14 @@ import PIL
 def postimg(image, title, description):
     client = Imgur({
         "client_id":        getenv("I_CLIENT_ID"), 
-        "access_token":        getenv("I_CLIENT_ID"), 
+        "access_token":     getenv("I_CLIENT_ID"), 
         "client_secret":    getenv("I_CLIENT_SECRET")
     }) 
 
     if type(image) is PIL.JpegImagePlugin.JpegImageFile:
-        image.save("/tmp/wojakimg.jpg")
-        image = "/tmp/wojakimg.jpg"
+        title = description.replace(' ', '')
+        image.save(f"/tmp/my_imgur_python/{title}.jpg")
+        image = f"/tmp/my_imgur_python/{title}.jpg"
 
     image = client.image_upload(path.realpath(image), title, description)
     url = "https://i.imgur.com/" + image["response"]["data"]["id"] + ".jpg"

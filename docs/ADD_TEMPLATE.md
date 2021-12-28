2990bed0d39ce50b8af9371576b85ecdb6f385dd
Pablo Ferreiro <pferreiromero@gmail.com>
2021-12-29 00:22:40 +0100

    Better text parsing

--- docs/ADD_TEMPLATE.md
+++ docs/ADD_TEMPLATE.md
@@ -1,6 +1,7 @@
 # Add template
 
 ## Structure
+
 This project use the following structure for a template:
 
     .
@@ -11,14 +12,17 @@ The folder containing all this files will be used to access it.
 MAKE SURE TO ADD YOUR TEMPLATE TO THE list.json FILE
 
 ## Config file
+
 The JSON file needs to have the following structure
+
 ```js
 {
-  "texts": [
-    {
-      "pos": [0, 0], // Initial position of text. First element of array is for the x axis and the second one is for the y axis.
-      "size": [360, 120], // Max width and height the text can use
-    }
-  ]
+    "texts": [{
+        "font_name": "Impact", // Font Family name
+        "font_size": 50,
+        "lw_multiplier": 2.5,
+        "pos": [0, 0], // Initial position of text. First element of array is for the x axis and the second one is for the y axis.
+        "width": 360, // Max width the text can use
+    }]
 }
 ```
--- wojak_generator/render.py
+++ wojak_generator/render.py
@@ -9,57 +9,31 @@ class PhotoRender:
         self.config = config
         self.image = Image.open(path)
 
-    def get_text_size(self, font_filename, font_size, text):
-        font = ImageFont.truetype(font_filename, font_size)
-        return font.getsize(text)
-
-    def get_font_size(self, text, font, max_width=None, max_height=None):
-        if max_width is None and max_height is None:
-            raise ValueError('You need to pass max_width or max_height')
-        font_size = 1
-        text_size = self.get_text_size(font, font_size, text)
-        if (max_width is not None and text_size[0] > max_width) or \
-           (max_height is not None and text_size[1] > max_height):
-            raise ValueError("Text can't be filled in only (%dpx, %dpx)" % \
-                    text_size)
-        while True:
-            if (max_width is not None and text_size[0] >= max_width) or \
-               (max_height is not None and text_size[1] >= max_height):
-                return font_size - 1
-            font_size += 1
-            text_size = self.get_text_size(font, font_size, text)
-
-    def writeText(self, xy: tuple, wh: tuple, font_filename: str, text: str):
-        # Get appropiate font size
-        font_size = self.get_font_size(text, font_filename, wh[0], wh[1])
-        # Create font with needed size
-        font = ImageFont.truetype(font_filename, font_size)
-        # Create string from textwrap
-        text_lines = '\n'.join(textwrap.wrap(text, wh[0]))
-        # Create empty image with 10px of margin and paste text on it
-        container_img = Image.new('RGB', (wh[0] + 10, wh[1] + 10), (255,255,255))
-        container_draw = ImageDraw.Draw(container_img)
-        container_draw.text((0, 0), text_lines, font=font, fill='black')
-        self.image.paste(container_img, xy)
+    def writeText(self, xy: tuple, width: int, font_filename: str, font_size: int, lw_multiplier: int, text: str):
+        img_editable = ImageDraw.Draw(self.image)
+        font_draw = ImageFont.truetype(font_filename, font_size)
+        line_width = lw_multiplier * width/font_size
+        text_lines = textwrap.fill(text, line_width)
+        img_editable.text(xy, text_lines, (0,0,0), font=font_draw)
 
     def run(self, texts: list):
         # Check if JSON is OK
         if 'texts' in self.config:
             for i in range(len(self.config["texts"])):
                 text_config = self.config["texts"][i]
-                if 'default' in text_config:
+                if 'default' in text_config and i > len(texts):
                     text = text_config["default"]
                 else:
                     text = texts[i]
                 font_filename = './fonts/DejaVuSans.ttf'
-                if 'font' in text_config:
-                    font_filename = f'./fonts/{text_config["font"]}.ttf'
-                self.writeText(tuple(text_config["pos"]), tuple(text_config["size"]), font_filename, text)
+                if 'font_name' in text_config:
+                    font_filename = f'./fonts/{text_config["font_name"]}.ttf'
+                self.writeText(tuple(text_config["pos"]), text_config['width'], font_filename, text_config['font_size'], text_config['lw_multiplier'], text)
 
     def saveToDisk(self, file: str):
         self.image.save(file, format='JPEG')
 
-    def getImage(self)-> Image.Image:
+    def getImage(self) -> Image.Image:
         return image
 
     def cleanup(self):
